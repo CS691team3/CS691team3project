24b0631c316b7a9770e8f2a717c1bcb39795e696
Kenneth Okereke <kennethokereke@Kenneths-MacBook-Pro.local>
2019-05-14 17:46:29 -0400

    IOS update

--- project1CS691-IOS-Version/Pods/Target Support Files/Pods-project1CS691/Pods-project1CS691-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_project1CS691 : NSObject
-@end
-@implementation PodsDummy_Pods_project1CS691
-@end
--- project1CS691-IOS-Version/Pods/Target Support Files/Pods-project1CS691Tests/Pods-project1CS691Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_project1CS691Tests : NSObject
-@end
-@implementation PodsDummy_Pods_project1CS691Tests
-@end
